b0f193257cc61385a1f5ad5ae32da8391a673bae
Manish Kumar <manish.kumar@elucidata.io>
2020-08-11 19:40:45 +0530

    added postgresql as database

--- fdp/settings.py
+++ fdp/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'fdp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photos_app',
+        'HOST': 'localhost'
     }
 }
 
